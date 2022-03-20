ed66bc5a3ceaa9f8e912c9f7b99ca6ce2134176b
Leonadon <84978080+khaizinam@users.noreply.github.com>
2022-03-20 20:26:17 +0700

    20/3

---
+++ public/assets/js/trang-chu-3.js
@@ -0,0 +1,23 @@
+var mes = {
+    1: {
+        1: {
+            1: {
+                link: 'index.php?cate=gamepab',
+                property: '',
+                content: 'Game Pab'
+            },
+            2: {
+                link: 'index.php?cate=book',
+                property: '',
+                content: 'Sách'
+            },
+        },
+        2: {
+            1: {
+                link: 'index.php?cate=all',
+                property: `class="bold"`,
+                content: 'Linh Tinh'
+            },
+        }
+    }
+}
---
+++ server/updatePage/search.php
@@ -0,0 +1,21 @@
+<?php 
+    include "../../config/config.php";
+    include "../header/conn.php";
+    $db = new DataBase();
+    $search = $_GET['search'];
+    $sql = "SELECT * 
+        FROM product 
+        WHERE name LIKE '%$search%' OR ID LIKE '%$search%'
+        ORDER BY ID ASC LIMIT 5"; 
+    $res = $db->select($sql);
+    $count = mysqli_num_rows($res);
+    if($count>0){
+        $data = array();
+        while($rows = $res->fetch_array()){
+            array_push($data, array('id' => $rows['ID'],
+            "name" => $rows['name']));
+        }
+        $json = json_encode($data);
+        echo $json;
+    }else echo "no data find";
+?>
