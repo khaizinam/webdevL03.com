5b1d4fd35545485a117ee852541c646dab813dd4
Leonadon <84978080+khaizinam@users.noreply.github.com>
2022-05-02 21:19:14 +0700

    a

---
+++ view/register/register.js
@@ -0,0 +1,41 @@
+function check_password() {
+    var password = document.getElementById('password').value;
+    var confirm_pass = document.getElementById('confirm_pass').value;
+    var e = document.querySelectorAll('.form-control');
+    if (password == '' && confirm_pass == '') {
+        e[2].classList.remove('error');
+        e[2].childNodes[3].innerHTML = '';
+    } else if (password != confirm_pass) {
+        e[2].classList.add('error');
+        e[2].childNodes[3].innerHTML = 'Mật khẩu không khớp';
+    } else {
+        e[2].classList.remove('error');
+        e[2].childNodes[3].innerHTML = 'Mật khẩu hợp lệ';
+    }
+}
+
+function check_username(str) {
+    if (str.length == 0) {
+        document.getElementById("txt").innerHTML = "";
+        return;
+    } else {
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.onload = function() {
+            document.getElementById("txt").innerHTML = this.responseText;
+        }
+        xmlhttp.open("GET", "controller/check_register.php?username=" + str);
+        xmlhttp.send();
+    }
+}
+
+function Register() {
+    var e = document.querySelectorAll('.form-control');
+    if (e[2].childNodes[3].innerHTML != 'Mật khẩu không khớp' && document.getElementById("txt").innerHTML == "") {
+        alert("Successful!");
+        window.location.href = "../login/login.html";
+        return true;
+    } else {
+        alert("Thất bại!");
+        return false;
+    }
+}
