15cf76c8ba29a6e1b85e83819f5f0fd719c5da7b
Leonadon <84978080+khaizinam@users.noreply.github.com>
2022-05-09 01:37:21 +0700

    a

--- public/js/productPage.js
+++ public/js/productPage.js
@@ -112,28 +112,37 @@ function getCookie(cname) {
 //Title of page in categories
 //08/05/2022
 //Nguyen Huu Khai
-$.get("../../model/productPage/tieu-de.php", {
-        cate: $("#cate-https").val()
-    },
-    function(data, status) {
-        if (status === 'success') {
-            let ndata = JSON.parse(data);
-            console.log(ndata);
-            $("#h3-1").html(ndata[1]);
-            if (ndata[0] != "all") {
-                $("#menu-direct").html(`<a href="index.php?cate=all&page=1">Trang chủ</a>
-                <span>></span>
-                <span>${ndata[1]}</span>`);
-            }
-        }
-    });
+// $.get("../../model/productPage/tieu-de.php", {
+//         cate: $("#cate-https").val()
+//     },
+//     function(data, status) {
+//         if (status === 'success') {
+//             let ndata = JSON.parse(data);
+//             console.log(ndata);
+//             $("#h3-1").html(ndata[1]);
+//             if (ndata[0] != "all") {
+//                 $("#menu-direct").html(`<a href="index.php?cate=all&page=1">Trang chủ</a>
+//                 <span>></span>
+//                 <span>${ndata[1]}</span>`);
+//             }
+//         }
+//     });
 $.get("../../model/productPage/category.php", {},
     function(data, status) {
         if (status === 'success') {
             let ndata = JSON.parse(data);
             let mes = ``;
+            let cate = $("#cate-https").val();
             console.log(ndata);
             for (let key in ndata) {
+                if (ndata[key].href == cate) {
+                    $("#h3-1").html(ndata[key].name);
+                    $("#menu-direct").html(`<a href="index.php?cate=all&page=1">Trang chủ</a>
+                    <span>></span>
+                    <span>${ndata[key].name}</span>`);
+                } else {
+                    $("#h3-1").html("Trang chủ");
+                }
                 mes += `<li><a href="index.php?cate=${ndata[key].href}">${ndata[key].name}</a> </li>`;
             }
             $("#show-list-categories").html(mes);
