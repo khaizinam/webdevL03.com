526845e7e9a39078b7df07243056bb9f0d1e2c99
Leonadon <84978080+khaizinam@users.noreply.github.com>
2022-05-09 21:29:44 +0700

    a

--- site/model/detailModel/detail_controller.php
+++ site/model/detailModel/detail_controller.php
@@ -1,14 +1,55 @@
- <?php
-    include "../../model/detailModel/DetailModel.php"; 
-    $detailmodel = new DetailModel();
-    if (isset($_GET['view'])) {
-        $res = $detailmodel->getDetail($_GET['view']);
-        $res = json_decode($res, true);
-        $comments = $detailmodel->getComments($_GET['view'], 1);
-        // print($comments);
-        $comments = json_decode($comments, true);
+<?php
+class DetailModel{
+    public $db;
+    public function __construct()
+    {
+        $this->db = new DataBase();
     }
-    else header("Location: ".$BASE_URL);
+    public function getDetail($id) {
+        $query = "SELECT * 
+            FROM product
+            WHERE `ID` = $id";
+        $num = $this->db->num($query);
+        if ($num > 0) {
+            $result = $this->db->send($query);
+            $row = $result->fetch_assoc();
+            $data = [
+                'id' => $id,
+                'name' => $row['name'],
+                'img' => $row['img'],
+                'amount' => $row['amount'],
+                'detail' => $row['detail'],
+                'price' => $row['price'],
+                'cate' => $row['cate']
+            ];
+            return json_encode($data);
+        } else {
+            return "{}";
+        }
+    }
+
+    public function getComments($id, $page) {
+        $query = "SELECT * FROM 
+            comment, user 
+            WHERE productID=$id AND page_id = $page AND comment.author_ID = user.ID";
+        $result = $this->db->send($query);
+        $data = [];
+        $num = $this->db->num($query);
+        if ($num > 0) {
+            while($row = $result->fetch_assoc()){
+                array_push($data,[
+                    'author_id' =>$row['author_ID'],
+                    'username' => $row['username'],
+                    'content' => $row['content']
+                ]);
+
+            }
+            return json_encode($data);
+        } else {
+            return "{}";
+        }
+    }
+}
 ?>
 
     
