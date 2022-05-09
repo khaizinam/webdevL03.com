7d45190bf7451470306d8b91f149925cc06bad1a
Leonadon <84978080+khaizinam@users.noreply.github.com>
2022-05-09 22:08:54 +0700

    a

--- site/view/detail/index_1.php
+++ site/view/detail/index_1.php
@@ -13,11 +13,11 @@
                         <ol class="breadcrumb">
                           <li class="breadcrumb-item"><a href="#" class="breadcrumb_product text-black">Trở lại</a></li>
                           <li class="breadcrumb-item"><a href="#" class="breadcrumb_product text-black">Trang chủ</a></li>
-                          <li class="breadcrumb-item active" aria-current="page"><?php echo $res['cate']?></li>
+                          <li class="breadcrumb-item active" aria-current="page"><?php //echo $res['cate']?></li>
                         </ol>
                       </nav>
                     <div class="product_image_details d-flex flex-wrap align-items-center justify-content-center">
-                        <img src="<?php echo $res['img']?>" class="product_image_inner" alt="img">
+                        <img src="../../../public/img/productimg/Audi203.png" class="product_image_inner" alt="img">
                     </div>
                 </div>
                 <div class="product_image_info">
@@ -26,7 +26,7 @@
                     </div>
                     <div class="row">
                         <div class="col-lg-12 col-xs-12 col-sm-12 col-md-12">
-                            <p class="product_detail"><?php echo $res['detail']?></p>
+                            <p class="product_detail"><?php //echo $res['detail']?></p>
                         </div>
                     </div>
 
@@ -35,38 +35,26 @@
                         <h6>3 Reviews</h6>
                     </div>
                     <div class="row">
-                        <div class="col-lg-4 col-xs-12 col-sm-12 col-md-12">
-                            <div class="rating ms-lg-4 d-flex align-items-center justify-content-center">
-                                <?php for($i=0; $i < $res['star']; $i++) { ?>
-                                    <i class="bi bi-star-fill"></i>
-                                <?php }?>
-                                <?php for($i=0; $i < 5-$res['star']; $i++) { ?>
-                                    <i class="bi bi-star"></i>
-                                <?php }?>
-                                <p class="rating_score"><?php echo $res['star']?>.0</p>
-                            </div>
-                        </div>
-                        <div class="col-lg-8 col-xs-12 col-sm-12 col-md-12">
-                            <?php foreach($comments as $key => $value) {?>
-                                
-                                <div class="comment">
-                                <p class="comment_title">
-                                    <?php echo $value['username']?>
-                                </p>
-                                <div class="comment_rating">
-                                    <?php for($i=0; $i < $value['star']; $i++) { ?>
-                                        <i class="bi bi-star-fill"></i>
-                                    <?php }?>
-                                    <?php for($i=0; $i < 5-$value['star']; $i++) { ?>
-                                        <i class="bi bi-star"></i>
-                                    <?php }?>
-                                </div>
+                        
+                        <div class="col-lg-8 col-xs-12 col-sm-12 col-md-12">          
+                            <div class="comment">
+                                <p class="comment_title">Nguyễn Hữu Khải</p>
                                 <div class="comment_content">
-                                    <?php echo $value['content']?>
+                                    TÔi rất đẹp chai
                                 </div>
-                            </div>
-                            <?php }?>
-                            
+                            </div> 
+                            <div class="comment">
+                                <p class="comment_title">Nguyễn Hữu Khải</p>
+                                <div class="comment_content">
+                                    TÔi rất đẹp chai
+                                </div>
+                            </div> 
+                            <div class="comment">
+                                <p class="comment_title">Nguyễn Hữu Khải</p>
+                                <div class="comment_content">
+                                    TÔi rất đẹp chai
+                                </div>
+                            </div> 
                         </div>
                     </div>
                 </div>
@@ -76,9 +64,9 @@
             <div class="col-lg-4 col-xs-12 col-sm-12 col-md-12 product_info">
                 <div class="container sticky-top">
                     <div class="product_info_pre_header">
-                        <span class="product_info_title"><?php echo $res['name']?></span>
-                        <p class="product_info_price"><?php echo $res['price']?></p>
-                        <p class="product_info_details">Số lượng trong kho: <?php echo $res['amount']?></b>
+                        <span class="product_info_title"><?php // echo $res['name']?></span>
+                        <p class="product_info_price"><?php // echo $res['price']?></p>
+                        <p class="product_info_details">Số lượng trong kho: <?php // echo $res['amount']?></b>
                         <p class="product_info_details"><b>No returns, no refunds</b><br>
                         Đơn hàng được xác nhận sau khi xác nhận thanh toán. Không đổi hàng, trả hàng hay hoàn tiền trừ trường hợp pháp luật có quy định khác. Sản phẩm này không áp dụng bất kỳ chương trình ưu đãi và khuyến mãi nào. Giới hạn số lượng 1 sản phẩm trên mỗi đơn hàng.</p>
                     </div>
