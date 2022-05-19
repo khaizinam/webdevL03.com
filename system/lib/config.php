11c1f8e8265c148f2ed2e5f6c3b6e5b69d93c78e
unknown <kirellia719@gmail.com>
2022-05-19 22:09:10 +0700

    .

--- system/lib/config.php
+++ system/lib/config.php
@@ -3,7 +3,7 @@ define('SITE_URL', '');
 define('HOSTNAME', 'localhost');
 define('USERTNAME', 'root');
 define('PASS', '');
-define('DATABASEBNAME', 'data_web');
+define('DATABASEBNAME', 'dataweb');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true ");
 header("Access-Control-Allow-Methods:GET, POST");
