bb89eb7aacd10ad98bfada344dce72c7d4e8adcf
Tran Minh Tan <82354057+gurenge1608@users.noreply.github.com>
2022-05-23 15:38:09 +0700

    comment

--- site/model/detailModel/DetailModel.php
+++ site/model/detailModel/DetailModel.php
@@ -21,11 +21,12 @@ class DetailModel extends DataBase{
     }
 
     public function getComments($id) {
-        $result = $this->send("SELECT * FROM comment, user WHERE comment.product_id=$id AND comment.author_ID = user.ID ORDER BY comment.ID DESC");
+        $result = $this->send("SELECT comment.ID, author_ID, username, content  FROM comment, user WHERE comment.product_id=$id AND comment.author_ID = user.ID ORDER BY comment.ID DESC");
         $data = [];
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()){
                 array_push($data,[
+                    'ID' => $row['ID'],
                     'author_id' =>$row['author_ID'],
                     'username' => $row['username'],
                     'content' => $row['content']
@@ -60,5 +61,23 @@ class DetailModel extends DataBase{
         $sql = "INSERT INTO comment (author_ID,product_id,content) VALUES ($userid, $productid, '$content')";
         if ($this->send($sql)) return true;
         else return false;
+    }
+
+    public function deleteComment($id) {
+        $sql = "DELETE FROM comment WHERE ID='$id'";
+        if ($this->send($sql)) return true;
+        else return false;
+    }
+
+    public function editComment($id, $content) {
+        $sql = "UPDATE comment SET content = '$content' WHERE ID='$id'";
+        if ($this->send($sql)) return true;
+        else return false;
+    }
+
+    public function getCommentbyID($id) {
+        $sql = "SELECT * FROM comment WHERE ID = '$id'";
+        $result = $this->send($sql);
+        return json_encode($result->fetch_assoc());
     }
 }
